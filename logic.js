470bd44923863cb3956dc575dd417f033e201f26
seoky99 <seokyoung99@gmail.com>
2024-09-24 16:27:32 -0400

    calculator num/op evaluate logic added

--- logic.js
+++ logic.js
@@ -1,11 +1,10 @@
 //global display variables 
-let fNumber;
-let operator;
-let sNumber; 
+let fNumber = null;
+let op = null;
+let sNumber = null; 
 
 //displayNum ref
 let displayValue = "";
-let display = document.querySelector("#text");
 
 function add(a, b) {
     return a + b;
@@ -27,23 +26,90 @@ function operate(a, b, op) {
     return op(a, b);
 }
 
-//handling click events 
+function changeDisplay(changeTo) {
+    displayValue = changeTo; 
+    let display = document.querySelector("#text");
+    display.textContent = displayValue;  
+}
+
+//responsive action on click, lowers opacity for 1 second
+function changeOpacityOnClick(e) {
+    e.target.style.opacity = 0.8;
+    setTimeout(() => {e.target.style.opacity = 1;}, 100);
+}
+
+function changeOperator(id) {
+    switch (id) {
+        case "add":
+            return add; 
+        case "subtract":
+            return subtract; 
+        case "divide":
+            return divide; 
+        case "multiply":
+            return multiply; 
+        default: 
+            throw new Error(); 
+    }
+}
+
+
+
+//handling click events AND logic for clicking 
 let delegator = document.querySelector(".calc-container");
 
 delegator.addEventListener("click", (e) => {
 
     if (e.target.className === "num") {
 
-        displayValue = displayValue + e.target.id; 
+        if (op == null) {
 
-        e.target.style.opacity = 0.8;
-        setTimeout(() => {e.target.style.opacity = 1;}, 100);
+            fNumber = fNumber == null ? e.target.id : fNumber + e.target.id;
+            changeDisplay(fNumber);
 
-        console.log(displayValue);
-        display.textContent = displayValue; 
+        } else {
 
-        console.log(e.target.id);
-        console.log(typeof(e.target.id))
-    }
+            sNumber = sNumber == null ? e.target.id : sNumber + e.target.id;
+            changeDisplay(sNumber);
+
+        }
+
+        changeOpacityOnClick(e); 
+
+    } else if (e.target.className === "op") {
+
+        if (e.target.id ==="clear") {
+
+            fNumber=null;
+            sNumber=null;
+            op=null;
+            changeDisplay("0");
+            console.log("here");
+
+        } else if (sNumber==null) {
+       
+            op = changeOperator(e.target.id);
+
+
+        } else {
+
+            fNumber = (operate(Number(fNumber), Number(sNumber), op)).toString(); 
+            sNumber = null; 
+            op = changeOperator(e.target.id);
+            changeDisplay(fNumber);
+
+        }
+
+        changeOpacityOnClick(e); 
+
+    } else if (e.target.className==="equals") {
+
+        if (sNumber != null && op != null) {
+            fNumber = (operate(Number(fNumber), Number(sNumber), op)).toString(); 
+            sNumber = null; 
+            op = null;
+            changeDisplay(fNumber);
+        }
+    } 
 
 })
