98610a0b4d293655767d5daa738568ebe4d08e54
seoky99 <seokyoung99@gmail.com>
2024-09-24 00:32:09 -0400

    changed to buttons and added events on the buttons to display clicked nums

--- logic.js
+++ logic.js
@@ -3,6 +3,10 @@ let fNumber;
 let operator;
 let sNumber; 
 
+//displayNum ref
+let displayValue = "";
+let display = document.querySelector("#text");
+
 function add(a, b) {
     return a + b;
 }
@@ -23,4 +27,23 @@ function operate(a, b, op) {
     return op(a, b);
 }
 
-console.log(operate(2, 5, divide));
+//handling click events 
+let delegator = document.querySelector(".calc-container");
+
+delegator.addEventListener("click", (e) => {
+
+    if (e.target.className === "num") {
+
+        displayValue = displayValue + e.target.id; 
+
+        e.target.style.opacity = 0.8;
+        setTimeout(() => {e.target.style.opacity = 1;}, 100);
+
+        console.log(displayValue);
+        display.textContent = displayValue; 
+
+        console.log(e.target.id);
+        console.log(typeof(e.target.id))
+    }
+
+})
